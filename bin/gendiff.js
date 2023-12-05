c0cfc82ca7da51cf079376167e0e7289e397fdff
Oksana <ksenia.tkachenko.ua@gmail.com>
2023-12-05 14:32:32 +0700

    added help information on the utility

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,3 +1,11 @@
 #!/usr/bin/env node
 
-console.log('Welcome');
+import { program } from 'commander';
+
+program
+  .version('1.0.0')
+  .description(
+    'Compares two configuration files and shows a difference',
+  );
+
+program.parse();
