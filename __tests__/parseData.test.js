2e09a56d50ef93392bf69fbbe1e924000a45d8d2
Oksana <ksenia.tkachenko.ua@gmail.com>
2023-12-06 19:47:27 +0700

    added Jest framework for code testing

---
+++ __tests__/parseData.test.js
@@ -0,0 +1,12 @@
+import dataParse from '../src/parseData';
+
+const file1 = {
+  host: 'hexlet.io',
+  timeout: 50,
+  proxy: '123.234.53.22',
+  follow: false,
+};
+
+test('dataParse', () => {
+  expect(dataParse('__fixtures__/file1.json')).toEqual(file1);
+});
